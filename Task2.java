1e66fead57f0e768070b57a0671d6a5c7c95eaa1
Geethika Wijeratne <31357705+geethikw@users.noreply.github.com>
2017-08-26 18:27:26 -0400

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -10,9 +10,8 @@ public class Task2 {
 		String s;
 		Task2 t1=new Task2();
 		
-		System.out.print("Enter the String:");
+		System.out.print("Enter the String:");//getting input String of any length from console
 		in = new Scanner(System.in);
-		
 		s = in.next();
 		System.out.println("Sum:"+t1.Add(s));
 
@@ -20,19 +19,19 @@ public class Task2 {
 	}	
 
 	private int Add(String numbers) {
+		
 	int stsum = 0;
-	
 	String nums= numbers;
-	
 	if(nums.length() == 0) {	
 		return 0;	
-	}else {
-		String numb = nums.replaceAll("[\\\\r\\\\n]+", ",");
+	} else {
+		String numb = nums.replaceAll("[\\\\r\\\\n]+", ","); // replce \n in the string with , for splitting
 		String[] numlist=numb.split(",");			
 		for(int i=0; i < numlist.length; i++) {
 		stsum += Integer.parseInt(numlist[i]);
 		}
 	return stsum ;
+		
 	}
   }
 	
