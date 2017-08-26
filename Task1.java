57e99f2409651c2d8cfd14bdf4586a10aaaef647
Geethika Wijeratne <31357705+geethikw@users.noreply.github.com>
2017-08-26 18:18:00 -0400

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,12 +1,12 @@
 package Kata;
 
-//This file contains the kata task 1,2,3
+//This file contains the kata task 1st step with String test.
 
 import java.util.Scanner;
 
 public class Task1 {
 	static String test="1,4";
-	private static Scanner in;
+	//private static Scanner in;
 	public static void main(String[] args) {
 		Task1 t1=new Task1();
 		System.out.println("Sum:"+t1.Add(test));
@@ -16,9 +16,7 @@ public class Task1 {
 	private int Add(String numbers) {
 	int stsum = 0;
 	String[] numlist;
-	
 	String nums= numbers;
-	
 	if(nums.length() == 0) {	
 		return 0;	
 	}else {
