4f160a2cb5d907f9c89b457e84d4d6200729ecd3
Geethika Wijeratne <31357705+geethikw@users.noreply.github.com>
2017-08-26 18:23:35 -0400

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -5,25 +5,32 @@ package Kata;
 import java.util.Scanner;
 
 public class Task1 {
-	static String test="1,4";
-	//private static Scanner in;
+	static String test1="1,4,4,5";//test Strings
+	static String test2="0";
+	static String test3="1,4,4";
+
 	public static void main(String[] args) {
 		Task1 t1=new Task1();
-		System.out.println("Sum:"+t1.Add(test));
+		System.out.println("Sum:"+t1.Add(test1));
 		return;
 	}	
 
 	private int Add(String numbers) {
-	int stsum = 0;
+	int stsum = 0,length=0;
 	String[] numlist;
 	String nums= numbers;
 	if(nums.length() == 0) {	
 		return 0;	
 	}else {
-		numlist=nums.split(",");			
-		
-		for(int i=0; i < numlist.length; i++) {
-		stsum += Integer.parseInt(numlist[i]);
+		numlist=nums.split(",");
+		if(numlist.length > 3) { //limit to length maximum only to 3
+			length=3;
+		}else {
+			length=numlist.length;
+		}
+		for(int i=0; i < length; i++) {
+			stsum += Integer.parseInt(numlist[i]);
+			
 		}
 	return stsum ;
 	}
