57c365861e4486224a3417c02c148ee16a5ee9dd
Geethika Wijeratne <31357705+geethikw@users.noreply.github.com>
2017-08-26 22:57:08 -0400

    Add files via upload

--- Task2.java
+++ Task2.java
@@ -5,33 +5,31 @@ package Kata;
 import java.util.Scanner;
 
 public class Task2 {
-	private static Scanner in;
+	
+	static String test1="7,3\n7\n5,6,8";//test Strings
+	static String test2="0";
+	static String test3="1\n4\n4";
+
 	public static void main(String[] args) {
 		String s;
 		Task2 t1=new Task2();
-		
-		System.out.print("Enter the String:");//getting input String of any length from console
-		in = new Scanner(System.in);
-		s = in.next();
-		System.out.println("Sum:"+t1.Add(s));
-
+		System.out.println("Sum:"+t1.Add(test1));
 		return;
 	}	
 
 	private int Add(String numbers) {
-		
 	int stsum = 0;
+	
 	String nums= numbers;
 	if(nums.length() == 0) {	
 		return 0;	
-	} else {
-		String numb = nums.replaceAll("[\\\\r\\\\n]+", ","); // replce \n in the string with , for splitting
+	}else {
+		String numb = nums.replaceAll("[\\n]+", ",");// replce \n in the string with , for splitting
 		String[] numlist=numb.split(",");			
 		for(int i=0; i < numlist.length; i++) {
 		stsum += Integer.parseInt(numlist[i]);
 		}
 	return stsum ;
-		
 	}
   }
 	
